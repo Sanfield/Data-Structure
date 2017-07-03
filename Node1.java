fb9a9277bd14ce7d659cf943715797a2738e7365
Sanfield <www.1042824123@qq.com>
2017-07-03 22:46:08 +0800

    堆的节点，堆一种特殊的二叉树

---
+++ Node1.java
@@ -0,0 +1,15 @@
+package heap;
+
+public class Node {
+	private int iData;
+	public Node(int key){
+		iData=key;
+	}
+	public int getKey(){
+		return iData;
+	}
+	public void setKey(int id){
+		iData=id;
+	}
+
+}
