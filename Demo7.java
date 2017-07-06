4d070adcecb0c90c7e18a5c43c3c7a93cb7e4356
Sanfield <www.1042824123@qq.com>
2017-07-06 22:33:06 +0800

    牛课网试题7

---
+++ Demo7.java
@@ -0,0 +1,12 @@
+package HUAWEI;
+
+import java.util.Scanner;
+//È¡½üËÆÖµ
+public class Demo7 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		double num= sc.nextDouble();
+		int s= (int)(num+0.5);
+		System.out.println(s);
+	}
+}
