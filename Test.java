18e07ce262676645a916a9ede4d26f4e2523e10a
Sanfield <www.1042824123@qq.com>
2017-08-09 22:51:17 +0800

    测试类

---
+++ Test.java
@@ -0,0 +1,16 @@
+package borderview;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+	public int x;
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++){
+			Integer k = new Integer(i);
+		}
+	}
+
+}
